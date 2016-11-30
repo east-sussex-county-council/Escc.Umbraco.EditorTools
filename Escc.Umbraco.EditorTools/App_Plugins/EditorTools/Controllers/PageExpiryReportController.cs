a802bd901246a7ba93859132f09210258aaf8923
Cai Lehwald <cai.lehwald@eastsussex.gov.uk>
2016-11-30 14:42:33 +0000

    refactor page expiry code, save content to application cache, refactor page expiry html

--- Escc.Umbraco.EditorTools/App_Plugins/EditorTools/Controllers/PageExpiryReportController.cs
+++ Escc.Umbraco.EditorTools/App_Plugins/EditorTools/Controllers/PageExpiryReportController.cs
@@ -1,49 +1,83 @@
-﻿using System.Collections.Generic;
+﻿using Escc.Umbraco.EditorTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using Umbraco.Web;
 using Umbraco.Web.WebApi;
-using Escc.Umbraco.EditorTools.Models;
 
 namespace Escc.Umbraco.EditorTools.App_Plugins.EditorTools.Controllers
 {
     public class PageExpiryReportController : UmbracoAuthorizedApiController
     {
+        // instantiate the application cache to reduce data queries
+        ObjectCache cache = MemoryCache.Default;
         // create a list to store the non perishable pages
         List<NonPerishableContent> nonPerishableList = new List<NonPerishableContent>();
 
         public IEnumerable<NonPerishableContent> GetAllNonPerishable()
         {
+            // check the cache to see if the list already exists
+            nonPerishableList = cache["pageExpiry"] as List<NonPerishableContent>;
+
+            // if it doesnt exist then generate the list
+            if (nonPerishableList == null)
+            {
+                nonPerishableList = new List<NonPerishableContent>();
+                populateList();
+
+                // add the list and date generated to the cache 
+                var dateString = DateTime.Now.ToString("dd MMMM yyyy");
+                dateString += DateTime.Now.ToString(" h:mm:sstt").ToLower();
+                cache.Add("pageExpiry", nonPerishableList, System.Web.Caching.Cache.NoAbsoluteExpiration, null);
+                cache.Add("reportDate", dateString, System.Web.Caching.Cache.NoAbsoluteExpiration, null);
+            }
+            return nonPerishableList;
+        }
+
+        public IEnumerable<NonPerishableContent> GetRefreshReport()
+        {
+            nonPerishableList.Clear();
+            populateList();
+            return nonPerishableList;
+        }
+
+        public string GetReportDate()
+        {
+            return cache["reportDate"] as string;
+        }
+
+
+        private void populateList()
+        {
             // get all content at the root of the content tree
             var rootCollection = UmbracoContext.ContentCache.GetAtRoot();
+
             // for each node at the root
             foreach (var node in rootCollection)
             {
-                // get the nodes descendants
-                var descendants = UmbracoContext.Application.Services.ContentService.GetDescendants(node.Id);
+                // get the root node from the application context to get the expiry date
+                var applicationNode = UmbracoContext.Application.Services.ContentService.GetById(node.Id);
 
-                // for each descendant
-                foreach (var child in descendants)
+                // if the exiry date is null. add the node to the list
+                if (applicationNode.ExpireDate == null && applicationNode.HasPublishedVersion)
                 {
-                    // if the expiry date is null e.g. set not to expire
-                    if (child.ExpireDate == null)
+                    // get the nodes descendants
+                    var descendants = UmbracoContext.Application.Services.ContentService.GetDescendants(node.Id);
+                    nonPerishableList.Add(new NonPerishableContent(node.Id, node.Name, node.Url));
+                    // for each descendant
+                    foreach (var child in descendants)
                     {
-                        // get the node from the contentcache
-                        var publishedNode = UmbracoContext.ContentCache.GetById(child.Id);
-                        // if the node is not published
-                        if (publishedNode == null)
+                        var ContentCacheChild = UmbracoContext.ContentCache.GetById(child.Id);
+                        // if the expiry date is null and the child is in the content cache
+                        if (child.ExpireDate == null && ContentCacheChild != null)
                         {
-                            // add the node to the list, but with the url set to 'unpublished'
-                            nonPerishableList.Add(new NonPerishableContent(child.Id, child.Name, "unpublished"));
-                        }
-                        // if the node is published
-                        else
-                        {
+                            // get the node from the contentcache
                             // add that nodes id, name and url to the content list
-                            nonPerishableList.Add(new NonPerishableContent(child.Id, child.Name, publishedNode.Url));
+                            nonPerishableList.Add(new NonPerishableContent(child.Id, child.Name, ContentCacheChild.Url));
                         }
                     }
                 }
             }
-
-            return nonPerishableList;
         }
     }
-}
+} 
---
+++ Escc.Umbraco.EditorTools/App_Plugins/EditorTools/Models/NonPerishableContent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Escc.Umbraco.EditorTools.Models
+{
+    public class NonPerishableContent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public NonPerishableContent(int id, string name, string url)
+        {
+            Id = id;
+            Name = name;
+            Url = url;
+        }
+    }
+}
