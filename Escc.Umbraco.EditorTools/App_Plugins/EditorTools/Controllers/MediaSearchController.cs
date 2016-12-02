8429dc62a4d20a89e3cae59809c546a7da619e48
Cai Lehwald <cai.lehwald@eastsussex.gov.uk>
2016-12-02 15:49:57 +0000

    Prepare to add Media Search Code

---
+++ Escc.Umbraco.EditorTools/App_Plugins/EditorTools/Controllers/MediaSearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Escc.Umbraco.EditorTools.App_Plugins.EditorTools.Controllers
+{
+    public class MediaSearchController : Controller
+    {
+        // GET: MediaSearch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Escc.Umbraco.EditorTools/App_Plugins/EditorTools/Models/Media.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Escc.Umbraco.EditorTools.App_Plugins.EditorTools.Models
+{
+    public class Media
+    {
+    }
+}
