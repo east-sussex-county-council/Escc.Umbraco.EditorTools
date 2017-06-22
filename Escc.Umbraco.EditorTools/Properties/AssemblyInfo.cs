a38c0d87e8eba054ba43a05a7cd473b0c240816c
Cai Lehwald <cai.lehwald@eastsussex.gov.uk>
2017-06-22 15:31:50 +0100

    Project cleanup, modify nuspec and targets file

--- Escc.Umbraco.EditorTools/Properties/AssemblyInfo.cs
+++ Escc.Umbraco.EditorTools/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("umbracoTools")]
+[assembly: AssemblyTitle("Umbraco.EditorTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("umbracoTools")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Umbraco.EditorTools")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dda53ccd-f08f-414b-ab60-a50e1c75bc94")]
+[assembly: Guid("12c92728-c47b-4edb-b4b5-d3ca1aae0641")]
 
 // Version information for an assembly consists of the following four values:
 //
